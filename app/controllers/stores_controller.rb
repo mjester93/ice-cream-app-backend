f600dd736229d78234ea4f50d41427637b690d44
mjester93 <m.jester.93@gmail.com>
2020-08-13 11:11:33 -0400

    add store controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,15 @@
 class StoresController < ApplicationController
+
+    def index
+        stores = Store.all
+
+        render json: stores.to_json(
+            :except => [:created_at, :updated_at]
+        )
+    end
+
+    def show
+
+    end
+
 end
