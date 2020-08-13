210eea0ade20db4e3d13270153ad27e80cd46967
mjester93 <m.jester.93@gmail.com>
2020-08-13 10:58:55 -0400

    adding Ice Cream Store to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+# Destroying old data
+User.destroy_all
+IceCream.destroy_all
+Store.destroy_all
+IceCreamStore.destroy_all
+
+
 # Creating Users
 5.times do 
     User.create({username: Faker::Internet.unique.username, location: 'Washington, DC'})
@@ -19,3 +26,11 @@ Store.create({name: "Ice Cream Jubilee", location: "Washington, DC", latitude: 3
 Store.create({name: "Bon Matcha", location: "Washington, DC", latitude: 38.901166, longitude: -77.044741})
 Store.create({name: "Nicecream", location: "Washington, DC", latitude: 38.923176, longitude: -77.042345})
 Store.create({name: "Thomas Sweet", location: "Washington, DC", latitude: 38.909112, longitude: -77.064073 })
+
+
+# Adding Three Ice Creams to each store
+Store.all.each do |store|
+    3.times do 
+        IceCreamStore.create({store_id: store.id, ice_cream_id: IceCream.all.sample.id})
+    end
+end
