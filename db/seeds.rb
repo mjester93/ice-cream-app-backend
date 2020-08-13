3c3b402ffff2f6abae200036e0cdbff7ad61df87
mjester93 <m.jester.93@gmail.com>
2020-08-13 10:37:58 -0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 # Creating Users
 5.times do 
-    User.create({name: Faker::Internet.unique.username, location: 'Washington, DC'})
+    User.create({username: Faker::Internet.unique.username, location: 'Washington, DC'})
 end
 
-michael = User.create({name: 'michael', location: 'Washington, DC'})
-esther = User.create({name: 'esther', location: 'Washington, DC'})
+michael = User.create({username: 'michael', location: 'Washington, DC'})
+esther = User.create({username: 'esther', location: 'Washington, DC'})
 
+10.times do 
+    IceCream.create({flavor: Faker::Dessert.unique.flavor, calories: rand(100...200)})
+end
