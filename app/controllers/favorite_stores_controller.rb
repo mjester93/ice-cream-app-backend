8c411a062720ac7afb867a7bd2baced0384fdf79
mjester93 <m.jester.93@gmail.com>
2020-08-17 17:23:10 -0400

    favorite store

--- app/controllers/favorite_stores_controller.rb
+++ app/controllers/favorite_stores_controller.rb
@@ -1,2 +1,19 @@
 class FavoriteStoresController < ApplicationController
+    skip_before_action :logged_in?
+
+    def create
+        favorite_store = FavoriteStore.new(
+            user_id: params[:userId],
+            store_id: params[:storeId]
+        )
+
+        if favorite_store.valid?
+            favorite_store.save
+            render json: favorite_store
+        else
+            render json: {error: "there is an error"}
+        end
+    end
+
+
 end
