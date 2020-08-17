c3fcfb4d7df217699901487a67573bfabb436c35
mjester93 <m.jester.93@gmail.com>
2020-08-17 10:20:45 -0400

    adding location to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password)
+        params.permit(:username, :password, :location)
     end
 
 end
