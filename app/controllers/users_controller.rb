c2984ec818f07cf56b911107bb8db0054918becc
mjester93 <m.jester.93@gmail.com>
2020-08-17 10:17:34 -0400

    adding index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     skip_before_action :logged_in?
+
+    def index
+        users = User.all
+        render json: users
+    end
   
     def create
         user = User.new(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :ice_cream_stores
   resources :ice_creams
   resources :stores
-  resources :users, only: [:create]
+  resources :users, only: [:create, :index]
   
   post '/login', to: 'auth#create'
 end
